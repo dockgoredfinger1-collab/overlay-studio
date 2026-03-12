f94e84778f71202f25633bd35199a7a3e3cddd1a
dockgoredfinger1-collab <dockgoredfinger1@gmail.com>
2026-03-12 23:33:58 +0800

    Update builder.js

--- builder.js
+++ builder.js
@@ -1,44 +1,67 @@
-document.getElementById("addBtn").onclick = function(){
+let widgets=[]
 
-let url = document.getElementById("widgetURL").value
+document.getElementById("addBtn").onclick=function(){
+
+let url=document.getElementById("widgetURL").value
 if(!url) return
 
 createWidget(url)
 
 }
 
+document.getElementById("generateBtn").onclick=function(){
+
+let data=encodeURIComponent(JSON.stringify(widgets))
+
+let url=location.origin+"/overlay.html?data="+data
+
+document.getElementById("result").innerHTML=
+"<a target='_blank' href='"+url+"'>"+url+"</a>"
+
+}
+
 function createWidget(src){
 
-let preview = document.getElementById("preview")
+let preview=document.getElementById("preview")
 
-let widget = document.createElement("div")
-widget.className = "widget"
+let widget=document.createElement("div")
+widget.className="widget"
 
-let handle = document.createElement("div")
-handle.className = "dragHandle"
-handle.innerText = "DRAG"
+let handle=document.createElement("div")
+handle.className="dragHandle"
+handle.innerText="DRAG"
 
-let iframe = document.createElement("iframe")
-iframe.src = src
+let iframe=document.createElement("iframe")
+iframe.src=src
 
 widget.appendChild(handle)
 widget.appendChild(iframe)
 
 preview.appendChild(widget)
 
-dragWidget(widget, handle)
+let widgetData={
+src:src,
+x:100,
+y:100,
+w:300,
+h:200
+}
+
+widgets.push(widgetData)
+
+dragWidget(widget,handle,widgetData)
 
 }
 
-function dragWidget(el, handle){
+function dragWidget(el,handle,data){
 
 let startX=0
 let startY=0
 let startLeft=0
 let startTop=0
 
-handle.addEventListener("mousedown", start)
-handle.addEventListener("touchstart", start)
+handle.addEventListener("mousedown",start)
+handle.addEventListener("touchstart",start)
 
 function start(e){
 
@@ -53,11 +76,11 @@ startY=e.clientY
 startLeft=el.offsetLeft
 startTop=el.offsetTop
 
-document.addEventListener("mousemove", move)
-document.addEventListener("mouseup", stop)
+document.addEventListener("mousemove",move)
+document.addEventListener("mouseup",stop)
 
-document.addEventListener("touchmove", move)
-document.addEventListener("touchend", stop)
+document.addEventListener("touchmove",move)
+document.addEventListener("touchend",stop)
 
 }
 
@@ -76,18 +99,24 @@ y=e.clientY
 let dx=x-startX
 let dy=y-startY
 
-el.style.left=(startLeft+dx)+"px"
-el.style.top=(startTop+dy)+"px"
+let newLeft=startLeft+dx
+let newTop=startTop+dy
+
+el.style.left=newLeft+"px"
+el.style.top=newTop+"px"
+
+data.x=newLeft
+data.y=newTop
 
 }
 
 function stop(){
 
-document.removeEventListener("mousemove", move)
-document.removeEventListener("mouseup", stop)
+document.removeEventListener("mousemove",move)
+document.removeEventListener("mouseup",stop)
 
-document.removeEventListener("touchmove", move)
-document.removeEventListener("touchend", stop)
+document.removeEventListener("touchmove",move)
+document.removeEventListener("touchend",stop)
 
 }
 
