186da51a85a1c055f07d8714af0449c4c7c607c1
dockgoredfinger1-collab <dockgoredfinger1@gmail.com>
2026-03-12 23:31:02 +0800

    Update builder.js

--- builder.js
+++ builder.js
@@ -1,38 +1,7 @@
-let widgets = []
+document.getElementById("addBtn").onclick = function(){
 
-function addSaweria(){
-
-let input = document.getElementById("saweriaKey")
-if(!input){
-console.error("Input saweriaKey tidak ditemukan")
-return
-}
-
-let key = input.value.trim()
-if(!key){
-alert("Masukkan StreamKey Saweria dulu")
-return
-}
-
-let src = "https://saweria.co/widgets/alert?streamKey=" + key
-
-createWidget(src)
-
-}
-
-function addWidget(){
-
-let input = document.getElementById("widgetURL")
-if(!input){
-console.error("Input widgetURL tidak ditemukan")
-return
-}
-
-let url = input.value.trim()
-if(!url){
-alert("Masukkan link widget dulu")
-return
-}
+let url = document.getElementById("widgetURL").value
+if(!url) return
 
 createWidget(url)
 
@@ -42,18 +11,8 @@ function createWidget(src){
 
 let preview = document.getElementById("preview")
 
-if(!preview){
-console.error("Div preview tidak ditemukan")
-return
-}
-
-let div = document.createElement("div")
-div.className = "widget"
-
-div.style.left = "100px"
-div.style.top = "100px"
-div.style.width = "300px"
-div.style.height = "200px"
+let widget = document.createElement("div")
+widget.className = "widget"
 
 let handle = document.createElement("div")
 handle.className = "dragHandle"
@@ -62,108 +21,74 @@ handle.innerText = "DRAG"
 let iframe = document.createElement("iframe")
 iframe.src = src
 
-div.appendChild(handle)
-div.appendChild(iframe)
+widget.appendChild(handle)
+widget.appendChild(iframe)
 
-preview.appendChild(div)
+preview.appendChild(widget)
 
-enableDrag(div, handle)
-
-widgets.push({
-src:src,
-x:100,
-y:100,
-w:300,
-h:200
-})
-
-console.log("Widget ditambahkan:", src)
+dragWidget(widget, handle)
 
 }
 
-function enableDrag(el, handle){
+function dragWidget(el, handle){
 
-let startX = 0
-let startY = 0
-let startLeft = 0
-let startTop = 0
+let startX=0
+let startY=0
+let startLeft=0
+let startTop=0
 
-handle.addEventListener("mousedown", startDrag)
-handle.addEventListener("touchstart", startDrag)
+handle.addEventListener("mousedown", start)
+handle.addEventListener("touchstart", start)
 
-function startDrag(e){
+function start(e){
 
-if(e.type === "touchstart"){
-startX = e.touches[0].clientX
-startY = e.touches[0].clientY
+if(e.type==="touchstart"){
+startX=e.touches[0].clientX
+startY=e.touches[0].clientY
 }else{
-startX = e.clientX
-startY = e.clientY
+startX=e.clientX
+startY=e.clientY
 }
 
-startLeft = el.offsetLeft
-startTop = el.offsetTop
+startLeft=el.offsetLeft
+startTop=el.offsetTop
 
-document.addEventListener("mousemove", drag)
-document.addEventListener("mouseup", stopDrag)
+document.addEventListener("mousemove", move)
+document.addEventListener("mouseup", stop)
 
-document.addEventListener("touchmove", drag)
-document.addEventListener("touchend", stopDrag)
+document.addEventListener("touchmove", move)
+document.addEventListener("touchend", stop)
 
 }
 
-function drag(e){
+function move(e){
 
-let clientX
-let clientY
+let x,y
 
-if(e.type === "touchmove"){
-clientX = e.touches[0].clientX
-clientY = e.touches[0].clientY
+if(e.type==="touchmove"){
+x=e.touches[0].clientX
+y=e.touches[0].clientY
 }else{
-clientX = e.clientX
-clientY = e.clientY
+x=e.clientX
+y=e.clientY
 }
 
-let dx = clientX - startX
-let dy = clientY - startY
+let dx=x-startX
+let dy=y-startY
 
-let newLeft = startLeft + dx
-let newTop = startTop + dy
-
-el.style.left = newLeft + "px"
-el.style.top = newTop + "px"
+el.style.left=(startLeft+dx)+"px"
+el.style.top=(startTop+dy)+"px"
 
 }
 
-function stopDrag(){
+function stop(){
 
-document.removeEventListener("mousemove", drag)
-document.removeEventListener("mouseup", stopDrag)
+document.removeEventListener("mousemove", move)
+document.removeEventListener("mouseup", stop)
 
-document.removeEventListener("touchmove", drag)
-document.removeEventListener("touchend", stopDrag)
+document.removeEventListener("touchmove", move)
+document.removeEventListener("touchend", stop)
 
-}
-
-}
-
-function generateOverlay(){
-
-if(widgets.length === 0){
-alert("Belum ada widget")
-return
-}
-
-let data = encodeURIComponent(JSON.stringify(widgets))
-
-let url = location.origin + "/overlay.html?data=" + data
-
-let result = document.getElementById("result")
-
-if(result){
-result.innerHTML =
-"Link Overlay:<br><a target='_blank' href='"+url+"'>" + url + "</a>"
 }
 
 }
